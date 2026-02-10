039f65556c9af53b526b3366ca3fba80fde75e32
bcorro <bcorro@sweadenseguros.com>
2026-02-10 17:17:00 -0500

    Bradley Corro: Primer Commit del aplicativo instalado

---
+++ tata-skills-test/src/main/java/com/bradleycorro/tataskillstest/TataSkillsTestApplication.java
@@ -0,0 +1,13 @@
+package com.bradleycorro.tataskillstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TataSkillsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TataSkillsTestApplication.class, args);
+    }
+
+}
---
+++ tata-skills-test/src/test/java/com/bradleycorro/tataskillstest/TataSkillsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bradleycorro.tataskillstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TataSkillsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
